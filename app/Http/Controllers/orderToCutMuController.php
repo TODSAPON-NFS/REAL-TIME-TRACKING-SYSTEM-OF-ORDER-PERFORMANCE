1edf729c1d0c03bf49f186b99b79c32160dd9080
IAmMonmoy <iammonmoy@gmail.com>
2017-04-09 00:14:07 +0600

    completed orderToCutMu

--- app/Http/Controllers/orderToCutMuController.php
+++ app/Http/Controllers/orderToCutMuController.php
@@ -6,7 +6,7 @@ use App\ordertocut_mu;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
-class muController extends Controller
+class orderToCutMuController extends Controller
 {
     //
     public function showMu()
@@ -27,7 +27,7 @@ class muController extends Controller
                $dbvar = ordertocut_mu::find(1);
             }
             $dbvar->IncreasedConsumption = $dbvar->IncreasedConsumption+$temp;
-             $dbvar->output = $dbvar->output+1.60* $dbvar->IncreasedConsumption;
+             $dbvar->Output = $dbvar->Output+(1.60* $dbvar->IncreasedConsumption);
             $dbvar->save();
        }
        else{
@@ -41,11 +41,11 @@ class muController extends Controller
             $dbvar->IncreasedConsumption = $dbvar->IncreasedConsumption-$temp;
             if($dbvar->IncreasedConsumption < 0.0)
                 $dbvar->IncreasedConsumption = 0;
-            $dbvar->output = $dbvar->output-1.60* $dbvar->IncreasedConsumption;
+            $dbvar->Output = $dbvar->Output-(1.60* $dbvar->IncreasedConsumption);
             $dbvar->save();
        }
       
-       return redirect()->action('muController@showMu') ->with('error', 'error');
+       return redirect()->action('orderToCutMuController@showMu') ->with('error', 'error');
     }
     public function postMuFabricFault(Request $request)
     {
@@ -54,6 +54,7 @@ class muController extends Controller
        if($addOrDelete == 1)
        {
             $dbvar = ordertocut_mu::find(1);
+           
             if(empty($dbvar))
             {
                 $dbrow = new ordertocut_mu;
@@ -61,7 +62,7 @@ class muController extends Controller
                 $dbvar = ordertocut_mu::find(1);
             }
             $dbvar->FabricFault = $dbvar->FabricFault+$temp;
-             $dbvar->output = $dbvar->output+0.16*  $dbvar->FabricFault;
+             $dbvar->Output = $dbvar->Output+(0.16*  $dbvar->FabricFault);
             $dbvar->save();
        }
        else{
@@ -75,11 +76,11 @@ class muController extends Controller
             $dbvar->FabricFault = $dbvar->FabricFault-$temp;
             if($dbvar->FabricFault < 0.0)
                 $dbvar->FabricFault = 0;
-             $dbvar->output = $dbvar->output-0.16*  $dbvar->FabricFault;
+             $dbvar->Output = $dbvar->Output-(0.16*  $dbvar->FabricFault);
             $dbvar->save();
        }
       
-       return redirect()->action('muController@showMu') ->with('error', 'error');
+       return redirect()->action('orderToCutMuController@showMu') ->with('error', 'error');
     }
      public function postMuRollShortage(Request $request)
     {
@@ -95,7 +96,7 @@ class muController extends Controller
                 $dbvar = ordertocut_mu::find(1);
             }
             $dbvar->RollShortage = $dbvar->RollShortage+$temp;
-             $dbvar->output = $dbvar->output+0.93* $dbvar->RollShortage;
+             $dbvar->Output = $dbvar->Output+0.93* $dbvar->RollShortage;
             $dbvar->save();
        }
        else{
@@ -109,11 +110,11 @@ class muController extends Controller
             $dbvar->RollShortage = $dbvar->RollShortage-$temp;
             if($dbvar->RollShortage < 0.0)
                 $dbvar->RollShortage = 0;
-             $dbvar->output = $dbvar->output-0.93* $dbvar->RollShortage;
+             $dbvar->Output = $dbvar->Output-0.93* $dbvar->RollShortage;
             $dbvar->save();
        }
       
-       return redirect()->action('muController@showMu') ->with('error', 'error');
+       return redirect()->action('orderToCutMuController@showMu') ->with('error', 'error');
     }
      public function postProductionDamage(Request $request)
     {
@@ -129,7 +130,7 @@ class muController extends Controller
                 $dbvar = ordertocut_mu::find(1);
             }
             $dbvar->ProductionDamage = $dbvar->ProductionDamage+$temp;
-            $dbvar->output = $dbvar->output+0.15* $dbvar->ProductionDamage;
+            $dbvar->Output = $dbvar->Output+0.15* $dbvar->ProductionDamage;
             $dbvar->save();
        }
        else{
@@ -143,11 +144,11 @@ class muController extends Controller
             $dbvar->ProductionDamage = $dbvar->ProductionDamage-$temp;
             if($dbvar->ProductionDamage < 0.0)
                 $dbvar->ProductionDamage = 0;
-             $dbvar->output = $dbvar->output-0.15* $dbvar->ProductionDamage;
+             $dbvar->Output = $dbvar->Output-0.15* $dbvar->ProductionDamage;
             $dbvar->save();
        }
       
-       return redirect()->action('muController@showMu') ->with('error', 'error');
+       return redirect()->action('orderToCutMuController@showMu') ->with('error', 'error');
     }
      public function postUnstableCut(Request $request)
     {
@@ -163,7 +164,7 @@ class muController extends Controller
                 $dbvar = ordertocut_mu::find(1);
             }
             $dbvar->UnusableCutPcs = $dbvar->UnusableCutPcs+$temp;
-            $dbvar->output = $dbvar->output+0.79* $dbvar->UnusableCutPcs;
+            $dbvar->Output = $dbvar->Output+0.79* $dbvar->UnusableCutPcs;
             $dbvar->save();
        }
        else{
@@ -177,11 +178,11 @@ class muController extends Controller
             $dbvar->UnusableCutPcs = $dbvar->UnusableCutPcs-$temp;
             if($dbvar->UnusableCutPcs < 0.0)
                 $dbvar->UnusableCutPcs = 0;
-             $dbvar->output = $dbvar->output-0.79* $dbvar->UnusableCutPcs;
+             $dbvar->Output = $dbvar->Output-0.79* $dbvar->UnusableCutPcs;
             $dbvar->save();
        }
       
-       return redirect()->action('muController@showMu') ->with('error', 'error');
+       return redirect()->action('orderToCutMuController@showMu') ->with('error', 'error');
     }
       public function postCuttingMistake(Request $request)
     {
@@ -198,7 +199,7 @@ class muController extends Controller
             }
            // return $temp;
             $dbvar->CuttingMistake = $dbvar->CuttingMistake+$temp;
-            $dbvar->output = $dbvar->output+0.10* $dbvar->CuttingMistake;
+            $dbvar->Output = $dbvar->Output+0.10* $dbvar->CuttingMistake;
             $dbvar->save();
        }
        else{
@@ -212,10 +213,10 @@ class muController extends Controller
             $dbvar->CuttingMistake = $dbvar->CuttingMistake-$temp;
             if($dbvar->CuttingMistake < 0.0)
                 $dbvar->CuttingMistake = 0;
-             $dbvar->output = $dbvar->output-0.10* $dbvar->CuttingMistake;
+             $dbvar->Output = $dbvar->Output-0.10* $dbvar->CuttingMistake;
             $dbvar->save();
        }
       
-       return redirect()->action('muController@showMu') ->with('error', 'error');
+       return redirect()->action('orderToCutMuController@showMu') ->with('error', 'error');
     }
 }
