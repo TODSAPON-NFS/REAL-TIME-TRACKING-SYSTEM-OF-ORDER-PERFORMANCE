2d0c00703d72fb0bd3a1aca1b4d6243c68348392
jaggesher <jaggesher14@gmail.com>
2017-04-09 22:20:06 +0600

    Updating all model

--- app/ordertocut_cad.php
+++ app/ordertocut_cad.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class ordertocut_cad extends Model
 {
     //
+    /**
+     * Indicates if the model should be auto incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false; 
 }
