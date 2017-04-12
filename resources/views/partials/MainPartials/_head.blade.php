8cff03dcbf42553c9cde60a7fecad45843c5353b
IAmMonmoy <iammonmoy@gmail.com>
2017-04-12 20:17:04 +0600

    added boostrap file for offline

--- resources/views/partials/MainPartials/_head.blade.php
+++ resources/views/partials/MainPartials/_head.blade.php
@@ -3,16 +3,5 @@
 
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-
-<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<link href="http://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
-<link href="http://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
-<link href="{{ asset('css/main.css') }}" rel="stylesheet" >
-<link href="{{ asset('css/bootstrap-dropdownhover.min.css') }}" rel="stylesheet" >
-<link href="{{ asset('css/animate.min.css') }}" rel="stylesheet" >
-<link href="https://fonts.googleapis.com/css?family=Jim+Nightshade" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Ropa+Sans" rel="stylesheet">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<script src="{{ asset('js/main.js') }}"></script>
-<script src="{{ asset('js/bootstrap-dropdownhover.min.js') }}"></script>
+<link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" >
+<script src="{{ asset('js/bootstrap.min.js') }}"></script>
